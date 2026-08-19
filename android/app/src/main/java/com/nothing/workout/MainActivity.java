859f751c63cab955b4571f0448f5f1f737e03239
jakeunson <jakeunson@gmail.com>
2026-08-19 19:20:01 +0900

    fix: Z Flip 커버화면 멀티스타 전체화면 뷰 하단 회색 바 제거 및 엣지투엣지 블랙 배경 적용

--- android/app/src/main/java/com/nothing/workout/MainActivity.java
+++ android/app/src/main/java/com/nothing/workout/MainActivity.java
@@ -1,5 +1,19 @@
 package com.nothing.workout;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Window;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setStatusBarColor(Color.BLACK);
+        window.setNavigationBarColor(Color.BLACK);
+        window.setBackgroundDrawableResource(android.R.color.black);
+        WindowCompat.setDecorFitsSystemWindows(window, false);
+    }
+}
